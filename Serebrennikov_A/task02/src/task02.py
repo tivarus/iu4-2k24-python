04935e5097f9e9a7ff7da4979098c2cb09d27c6f
Andrey Serebrennikov <seradya@gmail.com>
2024-04-17 22:33:30 +0300

    fix errors, add file count

--- Serebrennikov_A/task02/src/task02.py
+++ Serebrennikov_A/task02/src/task02.py
@@ -14,37 +14,40 @@ class Symbols(Enum):
     L = '└── '
 
 
-def sortByAlphabet(inputStr):
+def sortByAlphabet(inputStr: str) -> str:
     return str(inputStr).casefold()
 
 
-def tree(path: Path, depth: int, curr_lvl: int, init_symb: str = '', result: list = []) -> list:
-
+def tree(path: Path, depth: int, curr_lvl: int, init_symb: str = '', result: list = [None], files: int = 0, dirs: int = 0) -> list:
     path_list = list(path.glob('*'))
     path_list.sort(key=sortByAlphabet)
-    symbols = [Symbols.T.value] * \
-        (len(path_list) - 1) + [Symbols.L.value]
+    symbols = [Symbols.T.value] * (len(path_list) - 1) + [Symbols.L.value]
 
     for symbol, path in zip(symbols, path_list):
-        if (path.is_dir() == True):
+        if path.is_dir():
+            dirs += 1
             result.append(
                 f'{Fore.WHITE}{init_symb}{symbol}{Fore.BLUE}{path.name}')
-            if (curr_lvl < depth) | (depth == 0):
+            if curr_lvl < depth or depth == 0:
                 init_symb_next = init_symb + \
                     (Symbols.B.value if (symbol == Symbols.T.value) else Symbols.S.value)
-                tree(path, depth, curr_lvl+1, init_symb_next, result)
+                count = tree(path, depth, curr_lvl+1, init_symb_next, result)
+                files += count[0]
+                dirs += count[1]
         else:
+            files += 1
             result.append(
                 f'{Fore.WHITE}{init_symb}{symbol}{Fore.GREEN}{path.name}')
 
-    if (curr_lvl == 1):
+    if curr_lvl == 1:
+        result = list(filter(None, result))
         print('\n'.join(result))
+        print(f'{dirs} directories, {files} files')
 
-    return result
+    return [files, dirs, result]
 
 
 def main():
-
     parser = argparse.ArgumentParser(description='Dir path for tree')
     parser.add_argument('-L', '--Length', default='0', help='Tree depth')
     parser.add_argument('RootDir', nargs='?', default='.', help='Tree path')
