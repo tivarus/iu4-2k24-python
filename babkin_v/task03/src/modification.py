93ceeaf90a2037b9c7c45caa4adb2e3944064419
Vladimir Babkin <vladimirbabkun@yandex.ru>
2024-04-17 21:24:22 +0300

    style:  fixed annotation for modificateFile - added output annotation

--- babkin_v/task03/src/modification.py
+++ babkin_v/task03/src/modification.py
@@ -1,7 +1,7 @@
 from types import Node, Frame
 
 
-def modificateFile(elems: list[list[Node], list[Frame]]):
+def modificateFile(elems: list[list[Node], list[Frame]]) -> list[list[Node], list[Frame]]:
     frames = elems[1]
     for frame in frames:
         frame.bones[0].position.to_zero()
