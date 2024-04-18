59b907e04fea9b96c4059402b1c5c141ada23beb
яфкенч <stibrylarnet@mail.ru>
2024-04-18 20:13:07 +0300

    d

--- Dolgova AV/task 3/task03.py
+++ Dolgova AV/task 3/task03.py
@@ -1,68 +1,80 @@
-
-filename = 'a_move_walkW.smd'
-
-mode = 'waiting'
-nodes = {}
-timeframes = []
-
-class Timeframe:
-    def __init__(self, time, node, x, y, z, pitch, yaw, roll):
-    	self.time = time
-    	self.node = node
-    	self.x = x
-    	self.y = y
-    	self.z = z
-    	self.pitch = pitch
-    	self.yaw = yaw
-    	self.roll = roll
+from dataclasses import dataclass
+from lib import print_file
 
 
-with open(filename, 'r') as file:
+def main():
+    filename = 'a_move_walkW.smd'
+    mode = 'waiting'
+    nodes = []
+    timeframes = []
+    trigger_first_point = 0
+
+    @dataclass
+    class Timeframe:
+        time: str
+        node: str
+        x: str
+        y: str
+        z: str
+        pitch: str
+        yaw: str
+        roll: str
+
+    @dataclass
+    class Node:
+        id: str
+        name: str
+        coord: str
+
+    with open(filename, 'r') as file:
+        for line in file:
+            if 'nodes' in line:
+                mode = 'nodes'
+            elif 'skeleton' in line:
+                mode = 'skeleton'
+            elif 'end' in line:
+                mode = 'waiting'
+
+            if mode == 'waiting':
+                i = 0
+
+            # print(mode)
+            # блок нодов
+            if mode == 'nodes':
+                if i == 1:
+                    splitted = line.split()
+                    # print(node)
+                    tokens = [word.strip() for word in line.split()]
+                    node = Node(*tokens)
+                    nodes.append(node)
+
+                i = 1
+
+            # блок координат
+            if mode == 'skeleton':
+                if i == 1:
+                    if 'time' in line:
+                        time = line.split()
+                        k, timestamp = [word.strip() for word in line.split()]
+                    else:
+                        splitted = line.split()
+                        # print(splitted)
+                        # a, b, c, d, e, f, g = [word.strip() for word in line.split()]
+                        tokens = [word.strip() for word in splitted]
+
+                        if tokens[0] == '0':
+                            if trigger_first_point == 0:
+                                first_point = Timeframe(timestamp, *tokens)
+                                trigger_first_point = 1
+                            timeframe = first_point
+                        else:
+                            timeframe = Timeframe(timestamp, *tokens)
+                        timeframes.append(timeframe)
+
+                        # print(a)
+                i = 1
+
+    print_file(nodes, timeframes)
 
 
-	for line in file:
-		if 'nodes' in line:
- 			mode = 'nodes'
-		elif 'skeleton' in line:
-			mode = 'skeleton'
-		elif 'end' in line:
-			mode = 'waiting'
-
-
-		if mode == 'waiting':
-			i=0
-
-		#print(mode) 
-# блок нодов
-		if mode == 'nodes':
-			if i == 1:
-				splitted = line.split()
-				#print(node)
-				k, v, a = [word.strip() for word in line.split()]
-				nodes[k] = v
-
-			i=1
-
-# блок координат
-		if mode == 'skeleton':
-			if i == 1:
-				if 'time' in line:
-					time = line.split()
-					k, timestamp = [word.strip() for word in line.split()]
-				else:
-					splitted = line.split()
-					#print(splitted)
-					a, b, c, d, e, f, g = [word.strip() for word in line.split()]
-					if a == '0':
-						timeframe = Timeframe(timestamp,a,0,0,0,0,0,0)
-					else:
-						timeframe = Timeframe(timestamp,a,b,c,d,e,f,g)
-					timeframes.append(timeframe)
-
-					print(a)
-			i=1
-			
-print(nodes)
-for timeframe in timeframes:
-	print(f"time:{timeframe.time}, node:{timeframe.node}, x:{timeframe.x}, y:{timeframe.y}, z:{timeframe.z}, pitch:{timeframe.pitch}, yaw:{timeframe.yaw}, roll:{timeframe.roll}")
-
+main()
